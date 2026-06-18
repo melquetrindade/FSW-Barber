2bba023c6cc6815c66f9787a22b86549e4c8dfb0
melquetrindade <melquetrindade654@gmail.com>
2026-06-18 10:57:55 -0300

    chore: add prettier tailwind plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nextjs.org",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
